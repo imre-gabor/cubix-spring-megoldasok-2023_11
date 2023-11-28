4299874d5e12226c3afbaf2b74d0d61fde660769
imre-gabor <gabor@aut.bme.hu>
2023-11-28 18:43:41 +0100

    solution at live 1

--- src/main/java/com/cubixedu/hr/sample/service/SmartEmployeeService.java
+++ src/main/java/com/cubixedu/hr/sample/service/SmartEmployeeService.java
@@ -2,6 +2,9 @@ package com.cubixedu.hr.sample.service;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,16 +25,42 @@ public class SmartEmployeeService implements EmployeeService {
 		double yearsWorked = ChronoUnit.DAYS.between(employee.getDateOfStartWork(), LocalDateTime.now()) / 365.0;
 		
 		Smart smartConfig = config.getSalary().getSmart();
-		if(yearsWorked > smartConfig.getLimit3())
-			return smartConfig.getPercent3();
 		
-		if(yearsWorked > smartConfig.getLimit2())
-			return smartConfig.getPercent2();
+		//fix, beégetett darabszámú limit esetén
+//		if(yearsWorked > smartConfig.getLimit3())
+//			return smartConfig.getPercent3();
+//		
+//		if(yearsWorked > smartConfig.getLimit2())
+//			return smartConfig.getPercent2();
+//		
+//		if(yearsWorked > smartConfig.getLimit1())
+//			return smartConfig.getPercent1();
+//		
+//		return 0;
 		
-		if(yearsWorked > smartConfig.getLimit1())
-			return smartConfig.getPercent1();
+		//akárhány limit-százelék páros kezelése
+		TreeMap<Double, Integer> limits = smartConfig.getLimits();
+
+		//1. megoldás for ciklussal
+//		Integer maxPercent = null;
+//		for(var entry: limits.entrySet()) {
+//			if(yearsWorked > entry.getKey())
+//				maxPercent = entry.getValue();
+//			else
+//				break;
+//		}		
+//		return maxPercent == null? 0 : maxPercent;
 		
-		return 0;
+		//2. megoldás streammel
+//		Optional<Double> optionalMax = limits.keySet().stream()
+//		.filter(k -> yearsWorked >= k)
+//		.max(Double::compare);
+//		
+//		return optionalMax.isEmpty() ? 0 : limits.get(optionalMax.get());
+		
+		//3. megoldás
+		Entry<Double, Integer> floorEntry = limits.floorEntry(yearsWorked);
+		return floorEntry == null ? 0 : floorEntry.getValue();
 	}
 
 }
